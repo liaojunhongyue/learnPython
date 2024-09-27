9b7fd9dfb53c04154a9d9a0511c6490190eca622
ronngrong <2421155310@qq.com>
2024-09-27 21:14:51 +0800

    第一次提交

---
+++ chapter-01/code01-03.py
@@ -0,0 +1,16 @@
+# 任务1：打印数字2024
+print(2024)
+
+# 任务2：打印字符串 我是mia
+
+print('我是mia')
+
+# 任务3：创建变量year，值为2024，打印变量year
+year = 2024
+print(year)
+
+# 任务4：在一行内打印多个内容
+print(year, '年')
+print(year, '年', sep='')
+
+
---
+++ chapter-01/code01-04.py
@@ -0,0 +1,10 @@
+# 任务1：创建一个变量name，获取用户输入的名字，打印变量name
+name = input('请输入你的名字：')
+print(name)
+
+# 任务2：创建变量age，获取用户输入的年龄，打印用户的出生年份
+age = input('请输入你的年龄：')
+age = int(age)
+year = 2024
+birth = year - age
+print(birth)

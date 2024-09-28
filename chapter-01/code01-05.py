9bfa3717d994bd2526036a5f66182d5a8202ce47
ronngrong <2421155310@qq.com>
2024-09-28 17:04:27 +0800

    第一章练习

---
+++ chapter-01/code01-05.py
@@ -0,0 +1,13 @@
+name = input('请输入您的姓名：')
+company = input('请输入您的公司：')
+title = input('请输入您的职位：')
+phone = input('请输入您的电话：')
+email = input('请输入您的邮箱：')
+print('**********************')
+print('姓名：%s' % name)
+print('公司：%s' % company)
+print('职位：%s' % title)
+print('电话：%s' % phone)
+print('邮箱：%s' % email)
+print('**********************')
+
